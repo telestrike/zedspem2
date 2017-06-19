ee89485ae6640ac00c5e01e91ce7ed88e20a60b2
telestrike <telestrikeBOT1@GMAIL.COM>
2017-06-19 23:54:39 +0430

    Update Poker.lua

--- plugins/Poker.lua
+++ plugins/Poker.lua
@@ -1,21 +1,19 @@
-﻿--Begin Poker.lua By @MahDiRoO
-do
-
-function run(msg, matches)
-local reply_id = msg['id']
-local text = '😐'
-if matches[1] == '😐' then
-    if is_sudo(msg) then
-return text
-end
-end 
-end
-return {
-patterns = {
-    "😐"
-},
-run = run
-}
-
-end
---End Poker.lua--
+--Begin Poker.lua By @MahDiRoO 
+2 do 
+3 
+ 
+4 function run(msg, matches) 
+5 local reply_id = msg['id'] 
+6 local text = '😐' 
+7 if matches[1] == '😐' then 
+8     if is_sudo(msg) then 
+9 return text 
+10 end 
+11 end  
+12 end 
+13 return { 
+14 patterns = { 
+15     "😐" 
+16 }, 
+17 run = run 
+18 } 
